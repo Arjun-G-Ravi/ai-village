0c6e6ff78ca02c8f3002993e85c398a278466fdc
Arjun <arjungravi1@gmail.com>
2024-02-02 21:02:44 +0530

    Wrote base code

---
+++ V_1/base_characters.py
@@ -0,0 +1,5 @@
+'''
+This module cotains base character of every person.'''
+
+
+base_character = {'John': 'John is always angry and thinks that he will turn to hulk at any moment. He is always impatient and tensed.'}
---
+++ V_1/main.py
@@ -0,0 +1,39 @@
+class Person:
+    def __init__(self, name, position, base_character, memory, relationship):
+        self.name = name
+        self.position = position
+        self.base_character = base_character
+        self.memory = memory
+        self.relationship = relationship
+        
+    def add_memory(self, mem, importance):
+        import heapq
+        # if importance>1: importance=1
+        # if importance<0: importance=0
+        
+        heapq.heappush(self.memory.data, [-importance, mem]) # minus changes default min heap to max heap
+        # should do something to give more importance to recent information
+    
+    def read_memory(self, n=3):
+        # should write a function to fetch relevant information, given a situation (grep should work?)
+        ''' reads top 'n' important memories'''
+        out = []
+        for i in range(n):
+            out.append(self.memory.data[i])
+        return out
+        
+class Memory:
+    def __init__(self):
+        self.data = []
+    
+        
+
+
+
+
+if __name__ == '__main__':
+    tom_character = 'Tom is always angry at everyone. He works at the mart.'
+    tom_memory = Memory()
+    p1 = Person('Tom', (0,0), tom_character, tom_memory, [i for i in range(10)])
+    
+        
