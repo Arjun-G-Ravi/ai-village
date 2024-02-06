e4a7467fb6e5b377777f0d154098e615c067f009
Arjun <arjungravi1@gmail.com>
2024-02-06 06:04:15 +0530

    Killer cow

--- V_1/ai_model.py
+++ V_1/ai_model.py
@@ -12,11 +12,11 @@ class LLM:
         {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_NONE"}]
 
         self.model = genai.GenerativeModel('gemini-pro', safety_settings)
-        self.generation_config=genai.types.GenerationConfig(candidate_count=1, max_output_tokens=700, temperature=0.7, top_p=0.5)
+        self.generation_config=genai.types.GenerationConfig(candidate_count=1, max_output_tokens=700, temperature=0.9, top_p=.9)
     
     def generate(self, inp):
         try:
-            response = self.model.generate_content(inp+"Answer in character in one to two sentences.", generation_config=self.generation_config)
+            response = self.model.generate_content(inp+"Answer in character in two sentences.", generation_config=self.generation_config)
         except:
             return 'Try Again'
         return response.text
@@ -25,5 +25,7 @@ class LLM:
 if __name__ == '__main__':
     llm = LLM()
     print(llm.generate('''You are John, a twelve year old boy who thinks that he are a super cool assasin.You always talk in a shady and suspesious manner, even if there isnt one.
-                       As he was walking home from school, he saw a cow killing the mayor of the town. The cow also gave him a scary look and threatened him to never tell this thing to anybody. 
-                       The cow even mooed at him. This scared the life out of John. John doesn't dare tell this story to anyone. What will he say to his brother?'''))
+                       As he was walking home from school, he saw a cow killing the mayor of the town. The cow also gave him a scary look and threatened him that is he tell anybody of the
+                       incident, then the cow will stomp him to death. Even the slightest clue and John is done. The cow even mooed at him. This scared the life out of John. 
+                       John doesn't is so scared that he is willing to lie about never seeing the incident. Later, his brother asks if hehas seen anything about the murder of the mayor?
+                       Will John tell the truth or will he lie to his brother?'''))
