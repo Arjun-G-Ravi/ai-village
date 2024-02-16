bab90bb93fb67e4b7da9b32a2297d05d60f4e65c
RandomGameCoder <89650809+RandomGameCoder@users.noreply.github.com>
2024-02-16 16:20:06 +0530

    Update gui.py
    -Centered scrolling added

--- V_1/gui.py
+++ V_1/gui.py
@@ -2,17 +2,18 @@ import pygame
 
 pygame.init()
 class Window:
-
+    #game attributes
     DRAGGING = False
     
     def __init__(self):
+        #create window with fullscreen
         self.SCREEN = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
-        
+        #take window width and height
         self.WINDOW_WIDTH,self.WINDOW_HEIGHT = pygame.display.get_window_size()
-
+        #instantiate the world
         self.WORLD = World()
         self.WORLD.set_texture(pygame.image.load("World_Texture_placeholder.png").convert())
-
+        #start the event loop
         self.event_loop()
 
     def event_loop(self):
@@ -27,35 +28,38 @@ class Window:
                         quit()
                 #click and drag
                 elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    #currently dragging the screen
                     self.DRAGGING = True
+                    #calculate the offset of coordinates moved by the mouse when dragging
                     mouse_pos = event.pos
                     x_offset = self.WORLD.X - mouse_pos[0]
                     y_offset = self.WORLD.Y - mouse_pos[1]
                 elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                    #currently not dragging the screen
                     self.DRAGGING = False
                 elif event.type == pygame.MOUSEMOTION:
                     if self.DRAGGING and (self.WORLD.WIDTH*self.WORLD.SCALE) > self.WINDOW_WIDTH:
+                        #calculation of world coordinates after dragging
                         pos = event.pos
                         x = pos[0] + x_offset
                         y = pos[1] + y_offset
+                        #updating world coordinates
                         self.WORLD.set_x(max(min(0,x),-self.WORLD.WIDTH * self.WORLD.SCALE + self.WINDOW_WIDTH))
                         self.WORLD.set_y(max(min(0,y),-self.WORLD.HEIGHT * self.WORLD.SCALE + self.WINDOW_HEIGHT))
                 elif event.type == pygame.MOUSEWHEEL:
+                    #calculation of new world scale
                     scale = min(max(1,self.WORLD.SCALE + event.precise_y),4)
+                    #calculation for new world coordinates
                     x,y = pygame.mouse.get_pos()
-                    
-                    #under development
-                    posx = (x - self.WORLD.X)*scale - x
-                    posy = (y - self.WORLD.Y)*scale - y
-                    
-                    self.WORLD.set_x(-posx/scale)
-                    self.WORLD.set_y(-posy/scale)
-                    
+                    posx = ((x - self.WORLD.X)//self.WORLD.SCALE)*scale - x
+                    posy = ((y - self.WORLD.Y)//self.WORLD.SCALE)*scale - y
+                    #updating world coordinates and world scale
+                    self.WORLD.set_x(max(min(-posx,0),-self.WORLD.WIDTH * scale + self.WINDOW_WIDTH))
+                    self.WORLD.set_y(max(min(-posy,0),-self.WORLD.HEIGHT * scale + self.WINDOW_HEIGHT))
                     self.WORLD.set_scale(scale)
-
-
+            #game updates
             self.update()
-
+            #game rendering
             self.render()
 
 
@@ -64,13 +68,13 @@ class Window:
         pass
     
     def render(self):
+        #render events
         self.SCREEN.fill("gray")
-
         self.WORLD.draw(self.SCREEN)
-
         pygame.display.update()
 
 class World:
+    #world attributes
     SCALE = 1
     MAX_SCALE = 4
     X = 0
@@ -81,24 +85,30 @@ class World:
     SCALED_TEXTURE = None
 
     def set_scale(self,scale):
+        #update scale of world
         self.SCALE = scale
         self.scale()
     
     def set_x(self,x):
+        #update x coordinate of world
         self.X = x
     
     def set_y(self,y):
+        #update y coordinate of world
         self.Y = y
     
     def draw(self, surface):
+        #render the world texture
         surface.blit(self.SCALED_TEXTURE, (self.X, self.Y))
     
     def set_texture(self, texture):
+        #set the texture for the world
         self.TEXTURE = texture
         self.scale()
         
     def scale(self):
-        self.SCALED_TEXTURE = pygame.transform.scale(self.TEXTURE, (self.WIDTH*self.SCALE, self.HEIGHT*self.SCALE))
+        #scale the texture for rendering
+        self.SCALED_TEXTURE = pygame.transform.scale_by(self.TEXTURE, self.SCALE)
 
 
 if __name__ == "__main__":
