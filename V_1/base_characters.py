d27288c62229d46adc64b34db505f6f7809524e1
Arjun G Ravi <96014619+Arjun-G-Ravi@users.noreply.github.com>
2024-03-15 05:10:07 +0000

    Oder

--- V_1/base_characters.py
+++ V_1/base_characters.py
@@ -1,5 +1,5 @@
 '''
-This module cotains base character of every person.'''
+This module cotains base character of every person as a dictionary.'''
 
 
 base_character = {'John': 'John is always angry and thinks that he will turn to hulk at any moment. He is always impatient and tensed.'}
