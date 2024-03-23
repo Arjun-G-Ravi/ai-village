f5fea550eb88ef7e9a42e9933e44811ae81e68bd
Govind S Sarath <govindssarath@gmail.com>
2024-03-23 21:43:05 +0530

    actions idea written

--- V_1/tasks.py
+++ V_1/tasks.py
@@ -9,7 +9,7 @@ def path(node, parent):
     while p is not None:
         l.append(p)
         p = parent[p]
-    return tuple(l)
+    return tuple(l[::-1])
 
 def pathfinder(start,end,invalid):
     open_queue = PriorityQueue()
@@ -27,77 +27,115 @@ def pathfinder(start,end,invalid):
         for i in ((0,1), (0,-1), (1,0), (-1,0)):
             child = (node[0]+i[0], node[1]+i[1])
             est = h(child, end)
-            if (child not in closed or child not in open_set) and child not in invalid:
+            if child not in closed and child not in open_set and child not in invalid:
                 cost[child] = cost[node] + 1 
                 open_queue.put((cost[child] + est, child))
                 open_set.add(child)
                 parent[child] = node
             elif child in open_set and cost[child] > cost[node] + 1:
-                pass
-    
+                open_queue.queue.remove((cost[child]+est, child))
+                cost[child] = cost[node] + 1
+                open_queue.put((cost[child]+est, child))
+                parent[child] = node
+    return []
 
-def WALK(entity, loc):
+def WALK():
+    #get a location and go to that location
     pass
 
-def SLEEP(entity, loc):
+def SLEEP():
+    #get location of bed and go to that location
+    #sleep in bed
     pass
 
-def EAT:
+def EAT():
+    #go to dining table
+    #eat food
     pass
     
-def TALK:
+def TALK():
+    #need to discuss
     pass
     
-def GO_TO_WORK:
+def GO_TO_WORK():
+    #get location of work place and go to that location
+    #do work
     pass
     
-def COME_HOME_FROM_WORK:
+def COME_HOME_FROM_WORK():
+    #get location of home and go to that location
     pass
     
-def GO_TO_MARKET:
+def GO_TO_MARKET():
+    #get location of market and go to that location
     pass
     
-def DO_SHOPPING:
+def DO_SHOPPING():
+    #get location of stall and go to that location
+    #shop items
     pass
     
-def COME_BACK_HOME:
+def COME_BACK_HOME():
+    #get location of home and go to that location
     pass
     
-def READ:
+def READ():
+    #get location of bookshelf and go to that location
+    #read book
     pass
     
-def FARM:
+def FARM():
+    #get location of farmland and go to that location
+    #do farming
     pass
     
-def COOK:
+def COOK():
+    #get location of kitchen and go to that location
+    #do cooking
     pass
     
-def SING:
+def SING():
+    #start singing
     pass
     
-def DANCE:
+def DANCE():
+    #start dancing
     pass
     
-def PLAY:
+def PLAY():
+    #need to discuss
     pass
     
-def EXCERCISE:
+def EXCERCISE():
+    #get location of park and go to that location
+    #do excercise
     pass
     
-def PRAY:
+def PRAY():
+    #get location of pray area and go to that location
+    #pray
     pass
     
-def BRUSH:
+def BRUSH():
+    #get location of bathroom and go to that location
+    #brush teeth
     pass
     
-def BATH:
+def BATH():
+    #get location of bathroom and go to that location
+    #bathe
     pass
     
-def WAKE_UP:
+def WAKE_UP():
+    #wake up from sleep
     pass
     
-def PLAY_VIDEO_GAMES:
+def PLAY_VIDEO_GAMES():
+    #get location of tv and go to that location
+    #play games
     pass
     
-def WATCH_TV:
+def WATCH_TV():
+    #get location of tv and go to that location
+    #watch tv
     pass
