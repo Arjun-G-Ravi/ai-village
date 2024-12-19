04d2e0f1376701596e309ed176a6de26cce33102
arjungravi1@gmail.com <arjungravi1@gmail.com>
2024-12-19 12:23:34 +0530

    removed sprites

--- V_1/test_code/base_characters.py
+++
@@ -1,9 +0,0 @@
-'''
-This module cotains base character of every person as a dictionary.'''
-
-
-base_character_dict = {
-'John': 'John is always angry. He hates talking to people and gets mad at them. He often curses at people and is always ready to go into a fight.',
-'Tom': 'Tom is an introvert who is obsessed about marvel movies. Though he does not want to talk much, he loves to talk about the latest of marvel movies and comics. He also loves watching movies and playing video games.',
-'Jane': 'Jane is very extrovert and loves to talk to people. She loves to interact and learn from people.' 
-}
